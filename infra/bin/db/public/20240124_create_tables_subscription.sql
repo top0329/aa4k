8434eb07b123d768e539fd1029e81d1747759124
top0329 <top000329@gmail.com>
2024-04-23 18:59:27 +0900

    fix: [サブスクリプション]テーブルに[説明]カラムを追加 (#348)

--- infra/bin/db/public/20240124_create_tables_subscription.sql
+++ infra/bin/db/public/20240124_create_tables_subscription.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS m_subscription(
     subscription_id character varying (36) not null
     , company_id character varying (36) not null
     , schema_name character varying (60) not null
+    , comment text
     , trial_start_date date
     , trial_end_date date
     , contract_start_date date
@@ -17,6 +18,7 @@ comment on table m_subscription is 'サブスクリプションテーブル';
 comment on column m_subscription.subscription_id is 'サブスクリプションID:契約ごとのID（UUID）';
 comment on column m_subscription.company_id is '会社ID:契約した会社ID（UUID）';
 comment on column m_subscription.schema_name is 'スキーマ名';
+comment on column m_subscription.comment is '説明';
 comment on column m_subscription.trial_start_date is 'トライアル開始予定日:トライアルを開始する予定日付（YYYY/MM/DD）';
 comment on column m_subscription.trial_end_date is 'トライアル終了予定日:トライアルを終了する予定日付（YYYY/MM/DD）';
 comment on column m_subscription.contract_start_date is '本契約開始日予定日:本契約を開始する予定日付（YYYY/MM/DD）';
