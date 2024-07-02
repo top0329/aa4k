2b3ec73d7581958744d46bcb6ebbfac86f216775
top0329 <top000329@gmail.com>
2024-07-03 08:36:15 +0900

    feat: アプリ生成_API開発　事前チェックAPI(プラグイン用の修正) (#456)

--- infra/lib/lambda/utils/getSubscriptionData/dao.ts
+++ infra/lib/lambda/utils/getSubscriptionData/dao.ts
@@ -13,17 +13,21 @@ export const selectSubscription = async (dbClient: Client, subscriptionId: strin
   ]
   let sql = "";
   sql += `select`;
-  sql += ` subscription_id`;
-  sql += ` , company_id`;
-  sql += ` , schema_name`;
-  sql += ` , to_char(trial_start_date, 'YYYY-MM-DD') as trial_start_date`;
-  sql += ` , to_char(trial_end_date, 'YYYY-MM-DD') as trial_end_date`;
-  sql += ` , to_char(contract_start_date, 'YYYY-MM-DD') as contract_start_date`;
-  sql += ` , to_char(contract_end_date, 'YYYY-MM-DD') as contract_end_date`;
+  sql += ` subscription.subscription_id`;
+  sql += ` , subdomain.company_id`;
+  sql += ` , subdomain.schema_name`;
+  sql += ` , to_char(subdomain.trial_start_date, 'YYYY-MM-DD') as trial_start_date`;
+  sql += ` , to_char(subdomain.trial_end_date, 'YYYY-MM-DD') as trial_end_date`;
+  sql += ` , to_char(subdomain.contract_start_date, 'YYYY-MM-DD') as contract_start_date`;
+  sql += ` , to_char(subdomain.contract_end_date, 'YYYY-MM-DD') as contract_end_date`;
   sql += ` from`;
-  sql += ` m_subscription`;
+  sql += ` m_subscription subscription`;
+  sql += ` inner join m_subdomain subdomain`;
+  sql += ` on subscription.subdomain = subdomain.subdomain`;
   sql += ` where`;
   sql += ` subscription_id = $1`;
+  sql += ` and subscription.start_date <= CURRENT_DATE`;
+  sql += ` and (subscription.end_date is null or subscription.end_date > CURRENT_DATE)`;
 
   return await dbClient.query(sql, pram);
 };
